5d4a396c75ff4c01251162a3ffd82022a2fff217
gshpychka <glibshpychka@gmail.com>
2017-02-09 19:08:17 +0200

    Bugfix

--- src/main/java/TwitterStreamReceiver.java
+++ src/main/java/TwitterStreamReceiver.java
@@ -35,9 +35,11 @@ class TwitterStreamReceiver {
         if(status.isRetweet()) {
             status = status.getRetweetedStatus();
             listener.setRETWEET_CCOUNT(listener.getRETWEET_CCOUNT() + 1);
+        } else {
+            listener.setTWEET_COUNT(listener.getTWEET_COUNT() + 1);
         }
         databaseWriter.writeTweet(new StatusPOJO(status, keyword));
-        listener.setTWEET_COUNT(listener.getTWEET_COUNT() + 1);
+
     }
 
 
